00d38cf2888c237a161decb1f4b203dcfd9c2d83
43031 <sumonsorder953@gmail.com>
2021-03-10 17:27:39 +0600

    First Step

---
+++ src/main/java/com/sumon/xyz/SpringDataJPAFilterSearch/SpringDataJpaFilterSearchApplication.java
@@ -0,0 +1,13 @@
+package com.sumon.xyz.SpringDataJPAFilterSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaFilterSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaFilterSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumon/xyz/SpringDataJPAFilterSearch/SpringDataJpaFilterSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumon.xyz.SpringDataJPAFilterSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaFilterSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
